a0216d21c1a2a090834b2f50e98229690d83754a
MakarCat <tsivikov.makar@gmail.com>
2020-12-23 21:42:03 +0300

    RandomCheck update

--- RandomGeneratorCheck.c
+++ RandomGeneratorCheck.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() 
 {
     float r = 0;
+    srand(time(NULL));
     for (int i=0; i<10000000; i++) {
         if (rand()%10000 % 2 == 0) {
             r++;
